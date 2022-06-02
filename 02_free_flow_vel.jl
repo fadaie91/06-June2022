835f75851beaefbfe8469b6cf1bb41ce8bff150a
Reyhane Fadaie <85972119+fadaie91@users.noreply.github.com>
2022-06-02 18:39:33 -0400

    Update 02_free_flow_vel.jl

--- 02_free_flow_vel.jl
+++ 02_free_flow_vel.jl
@@ -21,6 +21,7 @@ tracer_advection = WENO5()
 #tracer_advection = UpwindBiasedFifthOrder()
 
 
+
 underlying_grid = RectilinearGrid(arch,
                                   size=(320, 40), halo=(3, 3), 
                                   y = (-4, 4),
@@ -31,6 +32,7 @@ underlying_grid = RectilinearGrid(arch,
 h₀ = 0.1 # bump height
 L = 1 # bump width
 @inline h(y) = h₀ * exp(- y^2 / L^2)
+@inline seamount(x, y) = - 1 + h(y)
 @inline seamount(x, y) = - 1 + h(y)
 
 seamount_field = Field{Center, Center, Nothing}(underlying_grid)
@@ -65,6 +67,32 @@ for ib in immersed_boundaries
 
     
 
+
+
+
+
+### In this example of the paper the velocities were prescribe
+### which means that they won't evolve during the simulation
+### and using the function below we prescribe the velocities
+#velocities = PrescribedVelocityFields( v=V, w=W)
+  
+  model = HydrostaticFreeSurfaceModel(; grid,
+                                        tracer_advection,
+                                        momentum_advection,
+                                        #coriolis = FPlane(f=0.1),
+                                        tracers = :b,
+                                        #velocities = velocities,
+                                        buoyancy = nothing)
+
+B= Field{Center, Center, Center}(grid)   
+N² = 1
+#bᵢ(x, y, z) = N² * z
+#B(x, y, z) = N² + z                                   
+set!(B, (x, y, z) -> (N² + z) ) 
+mask_immersed_field!(B)
+
+U, V, W = model.velocities
+
 Ψ = Field{Center, Face, Face}(grid)
 
 h(y)    = h₀*exp(-y^2/L^2)
@@ -98,28 +126,10 @@ fill_halo_regions!(V, arch)
 fill_halo_regions!(W, arch)
 
 
-
-### In this example of the paper the velocities were prescribe
-### which means that they won't evolve during the simulation
-### and using the function below we prescribe the velocities
-velocities = PrescribedVelocityFields( v=V, w=W)
   
-  model = HydrostaticFreeSurfaceModel(; grid,
-                                        tracer_advection,
-                                        momentum_advection,
-                                        #coriolis = FPlane(f=0.1),
-                                        tracers = :b,
-                                        velocities = velocities,
-                                        buoyancy = BuoyancyTracer())
-
-B= Field{Center, Center, Center}(grid)   
-N² = 1
-#bᵢ(x, y, z) = N² * z
-#B(x, y, z) = N² + z                                   
-set!(B, (x, y, z) -> (N² * z) ) 
-mask_immersed_field!(B)
-  
-    set!(model, b = B, v = V)
+    set!(model, b = B,  w=W, v=V)
+    mask_immersed_field!(B)
+mask_immersed_field!(V) 
 
     simulation = Simulation(model; Δt=1e-3, stop_time=1)
     simulation.callbacks[:p] = Callback(progress, IterationInterval(10))
@@ -135,7 +145,6 @@ b_partial = b[1]
 b_full    = b[2]
 Δb = b_full .- b_partial
 
-
 v_partial = v[1]
 v_full    = v[2]
 Δv = v_full .- v_partial
@@ -143,44 +152,38 @@ v_full    = v[2]
 fig = Figure(resolution=(1200, 1800))
 
 partial_cell_title = @sprintf("PartialCellBottom with ϵ = %.1f", minimum_fractional_Δz)
-#ax_bp = Axis(fig[1, 2], title=partial_cell_title)
-#ax_bf = Axis(fig[2, 2], title="GridFittedBottom")
-#ax_bd = Axis(fig[3, 2], title="Difference (GridFitted - PartialCell)")
+ax_bp = Axis(fig[1, 2], title=partial_cell_title)
+ax_bf = Axis(fig[2, 2], title="GridFittedBottom")
+ax_bd = Axis(fig[3, 2], title="Difference (GridFitted - PartialCell)")
 
-ax_vp = Axis(fig[1, 2], title=partial_cell_title)
-ax_vf = Axis(fig[2, 2], title="GridFittedBottom")
-ax_vd = Axis(fig[3, 2], title="Difference (GridFitted - PartialCell)")
-
- #ax_vp = Axis(fig[1, 3])
- #ax_vf = Axis(fig[2, 3])
- #ax_vd = Axis(fig[3, 3])
+ax_vp = Axis(fig[1, 4], title=partial_cell_title)
+ax_vf = Axis(fig[2, 4], title="GridFittedBottom")
+ax_vd = Axis(fig[3, 4], title="Difference (GridFitted - PartialCell)")
 
 color = (:black, 0.5)
 linewidth = 3
 levels = 15
 
-#hmbp = heatmap!(ax_bp, b_partial)
-#contour!(ax_bp, b_partial; levels, color, linewidth)
-#Colorbar(fig[1, 1], hmbp, label="Buoyancy", flipaxis=false)
+hmbp = heatmap!(ax_bp, b_partial)
+contour!(ax_bp, b_partial; levels, color, linewidth)
+Colorbar(fig[1, 1], hmbp, label="Buoyancy", flipaxis=false)
 
-#hmbf = heatmap!(ax_bf, b_full)
-#contour!(ax_bf, b_full; levels, color, linewidth)
-#Colorbar(fig[2, 1], hmbf, label="Buoyancy", flipaxis=false)
+hmbf = heatmap!(ax_bf, b_full)
+contour!(ax_bf, b_full; levels, color, linewidth)
+Colorbar(fig[2, 1], hmbf, label="Buoyancy", flipaxis=false)
 
-#hmbd = heatmap!(ax_bd, Δb)
-#Colorbar(fig[3, 1], hmbd, label="Buoyancy", flipaxis=false)
-
+hmbd = heatmap!(ax_bd, Δb)
+Colorbar(fig[3, 1], hmbd, label="Buoyancy", flipaxis=false)
 
 hmvp = heatmap!(ax_vp, v_partial)
 contour!(ax_vp, v_partial; levels, color, linewidth)
-Colorbar(fig[1, 1], hmvp, label="Velocity", flipaxis=false)
+Colorbar(fig[1, 3], hmvp, label="Velocity", flipaxis=false)
 
 hmvf = heatmap!(ax_vf, v_full)
 contour!(ax_vf, v_full; levels, color, linewidth)
-Colorbar(fig[2, 1], hmvf, label="Velocity", flipaxis=false)
+Colorbar(fig[2, 3], hmvf, label="Velocity", flipaxis=false)
 
 hmvd = heatmap!(ax_vd, Δv)
-Colorbar(fig[3, 1], hmvd)
-
+Colorbar(fig[3, 3], hmvd, label="Velocity", flipaxis=false)
 
 display(fig)
