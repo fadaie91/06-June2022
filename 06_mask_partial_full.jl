4aa4a36c556973e7d2e101ef7be9953aaafa215b
Reyhane Fadaie <85972119+fadaie91@users.noreply.github.com>
2022-06-06 19:03:47 -0400

    Update 06_mask_partial_full.jl

--- 06_mask_partial_full.jl
+++ 06_mask_partial_full.jl
@@ -20,14 +20,14 @@ function show_mask(grid)
 end
 
 underlying_grid = RectilinearGrid(arch,
-                                         size=(640, 80), halo=(3, 3),
-                                         y = (-4, 4),
+                                         size=(32, 16), halo=(3, 3),
+                                         y = (-1, 1),
                                          z = (-1, 0),
                                          topology=(Flat, Periodic, Bounded))
 
 # A bump
-h₀ = 0.1 # bump height
-L = 1 # bump width
+h₀ = 0.25 # bump height
+L = 0.5 # bump width
 
 @inline h(y) = h₀ * exp(- y^2 / L^2)
 @inline seamount(x, y) = - 1 + h(y)
@@ -35,17 +35,17 @@ L = 1 # bump width
 seamount_field = Field{Center, Center, Nothing}(underlying_grid)
 set!(seamount_field, seamount)
 fill_halo_regions!(seamount_field)
-grid_with_seamount = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(seamount_field.data))
+grid_with_seamount_full = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(seamount_field.data))
 
-x, y, z, cc = show_mask(grid_with_seamount)
+x, y, z, ccf = show_mask(grid_with_seamount_full)
 
-plt_full = heatmap(y, z, interior(cc)[1,:,:]', xlabel = "y", ylabel = "z", title = "Masked Region_Full_cell")
+plt_full = heatmap(y, z, interior(ccf)[1,:,:]', xlabel = "y", ylabel = "z", title = "Masked Region_Full_cell")
 savefig(plt_full,"mask_fullcell.png")
 
 
-grid_with_seamount = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data,minimum_fractional_Δz=1))
+grid_with_seamount_partial = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data,minimum_fractional_Δz=0.2))
 
-x, y, z, cc = show_mask(grid_with_seamount)
+x, y, z, ccp = show_mask(grid_with_seamount_partial)
 
-plt_partial = heatmap(y, z, interior(cc)[1,:,:]', xlabel = "y", ylabel = "z", title = "Masked Region_Partial_cell")
+plt_partial = heatmap(y, z, interior(ccp)[1,:,:]', xlabel = "y", ylabel = "z", title = "Masked Region_Partial_cell")
 savefig(plt_partial,"mask_partialcell.png")
