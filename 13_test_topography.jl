a57d67ecec30f1cd6e88a792b5eb47712f6c5f12
Reyhane Fadaie <85972119+fadaie91@users.noreply.github.com>
2022-06-14 12:42:40 -0400

    Update 13_test_topography.jl

--- 13_test_topography.jl
+++ 13_test_topography.jl
@@ -36,29 +36,32 @@ L = 1 # bump width
 seamount_field = Field{Center, Center, Nothing}(grid)
 #grid_with_seamount = ImmersedBoundaryGrid(grid, GridFittedBoundary(seamount))
 grid_with_seamount = ImmersedBoundaryGrid(grid, GridFittedBottom(seamount_field.data))
+#grid = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data,minimum_fractional_Δz=0.2))
 
 
 ###
 
-Ny = grid_with_seamount.data.Ny
-topography_index = zeros(Int64, Ny)
-for (iyC, yC) in enumerate(grid_with_seamount.grid.yC[1:Ny])
-    print("y = ", yC, " z = ", grid_with_seamount.grid.zC[1], " ")
+#Ny = grid_with_seamount.data.Ny
+Ny = grid_with_seamount.Ny
 
-    izC = 1
-    test = true
-    while test && izC <= grid_with_seamount.grid.Nz
-    	  print(izC, " ")
-	  test =  immersed_cell(1, iyC, izC, grid_with_seamount)
-	  izC += 1
-    end
-    print(" ")
+topography_index = zeros(Int64, Ny)
+for (iyC, yC) in enumerate(grid_with_seamount.yC[1:Ny])
+    print("y = ", yC, " z = ", grid_with_seamount.zC[1], " ")
+
+    #izC = 1
+    #test = true
+    #while test && izC <= grid_with_seamount.Nz
+    #	  print(izC, " ")
+	 # test =  immersed_cell(1, iyC, izC, grid_with_seamount)
+	  #izC += 1
+    #end
+    #print(" ")
     
-    print(immersed_cell(1, iyC, 1, grid_with_seamount)," ")
-    print(immersed_cell(1, iyC, 2, grid_with_seamount),"\n")
+    #print(immersed_cell(1, iyC, 1, grid_with_seamount)," ")
+    #print(immersed_cell(1, iyC, 2, grid_with_seamount),"\n")
     #print(solid_node(Center(), Center(), Center(), 1,iyC,1, grid_with_seamount)," ")
     #print(solid_node(Center(), Center(), Center(), 1,iyC,2, grid_with_seamount),"\n")
-    topography_index[iyC] = izC - 1 
+    #topography_index[iyC] = izC - 1 
 end
 
 print(topography_index, "\n")
